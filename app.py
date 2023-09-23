ee28a12a3653a970a6c07a35445b1210fb4bef69
Danilo Moreira <danilo.cs08@gmail.com>
2023-09-22 23:12:24 -0300

    Now the code is refatored for working on SQLite database

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ from flask_restful import Api
 from resources.hotel import Hotels, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
 
     
 api.add_resource(Hotels, "/hoteis")
 api.add_resource(Hotel, "/hoteis/<string:hotel_id>")
 
 if __name__ == "__main__":
+    from sql_alchemy import database
+    database.init_app(app)
+
+    with app.app_context():
+        database.create_all()
+    
     app.run(debug=True)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Formacao_dev\hotel_API\venv
+set VIRTUAL_ENV=C:\Formacao_dev\hotel_API\hotel_API\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
